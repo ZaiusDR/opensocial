5a8f42f872329c6b4ade0df191050219709f71ce
Eduardo Suarez <eduardosuarez@gmail.com>
2020-05-03 19:55:05 +0200

    Add a new endpoint to the API

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
 resource "aws_api_gateway_rest_api" "backend" {
   name = "${var.environment}_api"
 }
+
+resource "aws_api_gateway_resource" "projects_resource" {
+  parent_id   = aws_api_gateway_rest_api.backend.root_resource_id
+  path_part   = "projects"
+  rest_api_id = aws_api_gateway_rest_api.backend.id
+}
