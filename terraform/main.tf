6490f2605b9a2befbf35538ac3db66e9cad66797
Eduardo Suarez <eduardosuarez@gmail.com>
2020-05-03 18:49:44 +0200

    Move to bootstrapping module and setup an initial resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-resource "aws_instance" "testing" {
-  ami           = "ami-06ce3edf0cff21f07"
-  instance_type = "t2.micro"
+resource "aws_api_gateway_rest_api" "backend" {
+  name = "${var.environment}_api"
 }
