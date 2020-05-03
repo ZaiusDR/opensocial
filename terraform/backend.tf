8370fb972bd96a2a80d44b0d23f0db2432f155ee
Eduardo Suarez <eduardosuarez@gmail.com>
2020-05-03 18:51:30 +0200

    Fix formatting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    region = "eu-west-1"
-    bucket = "opensocial-dev-open-social-state"
-    key = "terraform.tfstate"
+    region         = "eu-west-1"
+    bucket         = "opensocial-dev-open-social-state"
+    key            = "terraform.tfstate"
     dynamodb_table = "opensocial-dev-open-social-state-lock"
-    encrypt = true
+    encrypt        = true
   }
 }
--- terraform/bootstrap.tf
+++ terraform/bootstrap.tf
@@ -1,8 +1,8 @@
 module "terraform_state_backend" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.16.0"
-  namespace     = "Open Social"
-  stage         = "dev"
-  name          = "open-social"
-  attributes    = ["state"]
-  region        = "eu-west-1"
+  source     = "git::https://github.com/cloudposse/terraform-aws-tfstate-backend.git?ref=tags/0.16.0"
+  namespace  = "Open Social"
+  stage      = "dev"
+  name       = "open-social"
+  attributes = ["state"]
+  region     = "eu-west-1"
 }
