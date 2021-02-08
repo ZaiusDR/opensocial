eb646b28d78cabd80b10e7bf6af8f6cb9b5f7826
Eduardo Suarez <eduardosuarez@gmail.com>
2021-02-08 21:03:29 +0100

    Add additional fields to frontend

--- open-social-front/src/Project.js
+++ open-social-front/src/Project.js
@@ -1,7 +1,16 @@
 import React from "react";
-import "./Project.css"
+import "./Project.css";
+import {Line, CartesianGrid, XAxis, YAxis, AreaChart} from 'recharts';
 
 function Project(props) {
+  const data = [
+    {name: '2020-01', uv: 4, pv: 5, amt: 2400},
+    {name: '2020-02', uv: 7, pv: 8, amt: 3400},
+    {name: '2020-03', uv: 4, pv: 5, amt: 8400},
+  ];
+  // const data = props.project.last_commit_dates.map(commit_date =>{
+  //   commit
+  // })
   return (
     <li key={props.project.full_name} className="Project-item">
       <div className="Project">
@@ -9,10 +18,21 @@ function Project(props) {
           <a href={props.project.project_url}>{props.project.project_name}</a>
         </div>
         <div>Full Name: {props.project.full_name}</div>
-        <div>Stargazers: {props.project.stargazers}</div>
+        <div>Description: {props.project.description}</div>
+        <div>Open Issues: {props.project.open_issues}</div>
         <div>Watchers: {props.project.watchers}</div>
         <div>Created: {props.project.created}</div>
         <div>Updated: {props.project.updated}</div>
+        <div>Forks: {props.project.forks}</div>
+        <div>Language: {props.project.language ? props.project.language : 'N/A'}</div>
+        <div>Archived: {props.project.archived ? 'Yes' : 'No'}</div>
+        <div>Last Commit Dates: {props.project.last_commit_dates}</div>
+        <div><AreaChart test-id='CommitGraph' width={600} height={300} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        </AreaChart></div>
       </div>
     </li>
   )
--- open-social-front/src/Project.test.js
+++ open-social-front/src/Project.test.js
@@ -5,27 +5,40 @@ import Project from "./Project";
 const project = {
   project_name: 'project1',
   full_name: 'user1/project1',
-  stargazers: '34',
+  description: 'fake_description',
+  open_issues: '25',
   watchers: '17',
   project_url: 'https://github.com/user1/project1',
   created: '2018-09-12T16:30:35Z',
-  updated: '2020-03-28T18:27:57Z'
+  updated: '2020-03-28T18:27:57Z',
+  forks: '2313',
+  language: 'Python',
+  archived: false,
+  last_commit_dates: ['2020-11-23T19:50:55Z', '2020-11-23T19:50:11Z']
 }
 
 test('renders a project information', () => {
   const { getByText } = render(<Project project={project}/>);
   const projectName = getByText(project.project_name);
-  const projectFullName = getByText(project.full_name, { exact: false })
-  const stargazers = getByText(project.stargazers, { exact: false });
+  const projectFullName = getByText(project.full_name, { exact: false });
+  const description = getByText(project.description, { exact: false });
+  const openIssues = getByText(project.open_issues, { exact: false });
   const watchers = getByText(project.watchers, { exact: false });
   const created = getByText(project.created, { exact: false });
   const updated = getByText(project.updated, { exact: false });
+  const forks = getByText(project.forks, { exact: false });
+  const language = getByText(project.language, { exact: false });
+  const archived = getByText('No', { exact: false });
 
   expect(projectName).toBeInTheDocument();
   expect(projectName).toHaveAttribute('href', project.project_url);
   expect(projectFullName).toBeInTheDocument();
-  expect(stargazers).toBeInTheDocument();
+  expect(description).toBeInTheDocument();
+  expect(openIssues).toBeInTheDocument();
   expect(watchers).toBeInTheDocument();
   expect(created).toBeInTheDocument();
   expect(updated).toBeInTheDocument();
+  expect(forks).toBeInTheDocument();
+  expect(language).toBeInTheDocument();
+  expect(archived).toBeInTheDocument();
 });
