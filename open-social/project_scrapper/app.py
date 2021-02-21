9b3dd9c3cdf78640dd11cca565bf5d7e9ea74cc5
Eduardo Suarez <eduardosuarez@gmail.com>
2021-02-21 08:41:23 +0100

    Remove REST Implementation

--- open-social/project_scrapper/app.py
+++ open-social/project_scrapper/app.py
@@ -1,15 +1,11 @@
 import json
 
-from infrastructure import github_gateway
 from infrastructure import github_gateway_gql
 from service import project_service
 
 
 def lambda_handler(event, context):
-    if 'topic' in event:
-        github_projects = github_gateway_gql.get_project_list(event)
-    else:
-        github_projects = github_gateway.get_project_list(event)
+    github_projects = github_gateway_gql.get_project_list(event)
     projects = project_service.bulk_save(github_projects)
 
     return {
