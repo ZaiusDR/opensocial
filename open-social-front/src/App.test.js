f47869a59d77192a68cf4938e51f4470c6a17842
Eduardo Suarez <eduardosuarez@gmail.com>
2021-02-23 19:53:46 +0100

    Upgrade Frontend dependencies

--- open-social-front/src/App.test.js
+++ open-social-front/src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render, screen, waitForElement } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test('list projects from API', async () => {
   render(<App />);
-  const project = await waitForElement(() => screen.getByText(/user1\/project1/i));
+  const project = await waitFor(() => screen.getByText(/user1\/project1/i));
   expect(project).toBeInTheDocument();
 });
