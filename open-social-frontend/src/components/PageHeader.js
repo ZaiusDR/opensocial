d3ca081a51f0c9ca1b66f68a21d22f0ae3a7b3cd
Eduardo Suarez <eduardosuarez@gmail.com>
2021-03-20 08:53:21 +0100

    Make the logo and title a link to home

--- open-social-frontend/src/components/PageHeader.js
+++ open-social-frontend/src/components/PageHeader.js
@@ -9,15 +9,17 @@ import '../styles/PageHeader.css'
 
 function PageHeader() {
   return <Header className="Page-header" >
-    <div className="Logo-container" >
-      <StaticImage
-        src={'../images/icon.png'}
-        alt={'unicorn'}
-        height={44}
-        layout='fixed'
-      />
-      <h1 className="Logo-title">Open Social</h1>
-    </div>
+    <a href="https://www.open-social.net">
+      <div className="Logo-container" >
+        <StaticImage
+          src={'../images/icon.png'}
+          alt={'unicorn'}
+          height={44}
+          layout='fixed'
+        />
+        <h1 className="Logo-title">Open Social</h1>
+      </div>
+    </a>
   </Header>
 }
 
