e87cc2396048308222554b017dcd14da8b255bda
Eduardo Suarez <eduardosuarez@gmail.com>
2023-02-25 12:20:34 +0100

    Don't lazy load home images

--- open-social-front-v2/components/home/SubHeroItem.js
+++ open-social-front-v2/components/home/SubHeroItem.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 const SubHeroItem = (props) => {
   return (
     <div className="card shadow-xl overflow-hidden">
-      <figure><Image className="object-cover" src={props.imageUrl} alt="Shoes" width={600} height={400}/></figure>
+      <figure><Image className="object-cover" src={props.imageUrl} alt="Shoes" width={600} height={400} priority /></figure>
       <div className="card-body items-center">
         <h2 className="card-title">{props.title}</h2>
         <p>{props.subText}</p>
