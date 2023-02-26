8c2b10c43c06f436bf3900b22370ac5b69abb66a
Eduardo Suarez <eduardosuarez@gmail.com>
2023-02-26 23:11:23 +0100

    Revert "Add GTag"
    This reverts commit 097f72f9a5906674220e7929d9cff95967f4bd49.

--- open-social-front-v2/components/metadata/HeadSection.js
+++ open-social-front-v2/components/metadata/HeadSection.js
@@ -1,26 +1,13 @@
 import Head from "next/head"
-import Script from "next/script"
 
 const HeadSection = () => {
   return (
-    <>
-      <Head>
-        <title>OpenSocial - Find Social Impact Open Source Projects</title>
-        <meta name="description" content="OpenSocial is a social impact open source projects aggregator. Easily find active non profit projects and start coding a better world." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="google-site-verification" content="cHmuOEOke-_B5U7Ns3rJlBT-pTN4qipfs7W08LtEn2w" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Script id="gtag-manager" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MS7SCQ6');
-        `}
-      </Script>
-    </>
+    <Head>
+      <title>OpenSocial - Find Social Impact Open Source Projects</title>
+      <meta name="description" content="OpenSocial is a social impact open source projects aggregator. Easily find active non profit projects and start coding a better world." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
   )
 }
 
--- open-social-front-v2/pages/_document.js
+++ open-social-front-v2/pages/_document.js
@@ -7,8 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MS7SCQ6"
-                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       </body>
     </Html>
   )
