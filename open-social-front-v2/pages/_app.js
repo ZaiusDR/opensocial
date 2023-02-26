78ff672b3b44944381b98e707ed08b4e1af989e6
Eduardo Suarez <eduardosuarez@gmail.com>
2023-02-26 23:13:42 +0100

    Try setting up GTag with library

--- open-social-front-v2/pages/_app.js
+++ open-social-front-v2/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { useEffect } from "react"
+import TagManager from "react-gtm-module/dist/TagManager"
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MS7SCQ6' });
+  }, [])
+
   return <Component {...pageProps} />
 }
