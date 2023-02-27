bcc41488ea7937af8ca9ef276e2bd7c4ab41e242
Eduardo Suarez <eduardosuarez@gmail.com>
2023-02-27 16:17:22 +0100

    Add search console config

--- open-social-front-v2/components/metadata/HeadSection.js
+++ open-social-front-v2/components/metadata/HeadSection.js
@@ -6,6 +6,7 @@ const HeadSection = () => {
       <title>OpenSocial - Find Social Impact Open Source Projects</title>
       <meta name="description" content="OpenSocial is a social impact open source projects aggregator. Easily find active non profit projects and start coding a better world." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="google-site-verification" content="cHmuOEOke-_B5U7Ns3rJlBT-pTN4qipfs7W08LtEn2w" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
