b51aa6112132df49195c7c2b2081050b74faf2d2
Eduardo Suarez <eduardosuarez@gmail.com>
2023-03-01 20:38:18 +0100

    Adjust logo size for small screens

--- open-social-front-v2/components/layout/Logo.js
+++ open-social-front-v2/components/layout/Logo.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const Logo = (props) => {
   return(
     <Link href="/" className={`${props.visible} flex-0 btn btn-ghost px-2`}>
-      <div className="font-title font-extrabold text-lg md:text-3xl">
+      <div className="font-title font-extrabold text-3xl">
         <span className="capitalize text-primary">Open</span>
         <span className="capitalize text-base-content">Social</span>
       </div>
