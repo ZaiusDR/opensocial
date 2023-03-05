65fb71e031c4ee2d9ad21594ab1416174ee218b7
Eduardo Suarez <eduardosuarez@gmail.com>
2023-03-05 12:45:33 +0100

    Add Canonical link and Sitemap

--- open-social-front-v2/components/metadata/HeadSection.js
+++ open-social-front-v2/components/metadata/HeadSection.js
@@ -7,6 +7,7 @@ const HeadSection = () => {
       <meta name="description" content="OpenSocial is a social impact open source projects aggregator. Easily find active non profit projects and start coding a better world." />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="google-site-verification" content="cHmuOEOke-_B5U7Ns3rJlBT-pTN4qipfs7W08LtEn2w" />
+      <link rel="canonical" href="https://www.open-social.net" key="canonical" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
