215fb419d6619a7411a939c607f4f696662c1978
Eduardo Suarez <eduardosuarez@gmail.com>
2023-03-05 13:56:02 +0100

    Revert "Try with extra component"
    This reverts commit 03aac55aac28e7042ddc98f89855797755a1ae29.

--- open-social-front-v2/components/projects/ProjectRating.js
+++ open-social-front-v2/components/projects/ProjectRating.js
@@ -1,17 +1,16 @@
-import ProjectRatingStar from "@/components/projects/ProjectRatingStar"
-
 const ProjectRating = (props) => {
   return(
     <div className="rating rating-half">
-      {Array.apply(0, Array(10)).map((x, i) => {
+      {Array.apply(0, Array(10)).map(function (x, i) {
         const isChecked = props.rating >= (i + 1) / 10 && props.rating < (i + 2) / 10 ? { defaultChecked : true } : { defaultChecked : false }
 
-        return <ProjectRatingStar
+        return <input
           key={i}
-          id={`rating-${props.id}`}
-          index={i}
-          classType={`mask-half-${(i % 2) + 1}`}
-          isChecked={isChecked}
+          type="radio"
+          name={`rating-${props.id}`}
+          disabled
+          className={`bg-primary mask mask-star-2 mask-half-${i % 2 + 1}`}
+          {...isChecked}
         />;
       })}
     </div>
