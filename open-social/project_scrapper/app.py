46365cab92688cf754342af484b9a645d8f71c14
Eduardo Suarez <eduardosuarez@gmail.com>
2024-05-09 11:52:05 +0200

    Test MongoDB connection from Scrapper Lambda

--- open-social/project_scrapper/app.py
+++ open-social/project_scrapper/app.py
@@ -1,10 +1,34 @@
 import json
+import os
+
+import boto3
+import pymongo
 
 from infrastructure import github_gateway
 from service import project_service
 
 
+def _test_mongo_connection():
+    sts_client = boto3.client('sts')
+
+    response = sts_client.assume_role(
+        RoleArn='arn:aws:iam::326499071401:role/mongo-db-rw',
+        RoleSessionName='AssumeRole'
+    )
+    os.environ['AWS_ACCESS_KEY_ID'] = response['Credentials']['AccessKeyId']
+    os.environ['AWS_SECRET_ACCESS_KEY'] = response['Credentials']['SecretAccessKey']
+    os.environ['AWS_SESSION_TOKEN'] = response['Credentials']['SessionToken']
+
+    secrets_manager_client = boto3.client('secretsmanager')
+    uri = secrets_manager_client.get_secret_value(SecretId='mongodb-uri')
+
+    mongo_client = pymongo.MongoClient(uri)
+    mongo_client.get_database('open-social')
+
+
 def lambda_handler(event, context):
+    if event['topic'] == 'test':
+        _test_mongo_connection()
     github_projects = github_gateway.get_project_list(event)
     projects = project_service.bulk_save(github_projects)
 
