5c82bd0b94079cc722f6dc55daf627c7365be0c0
Eduardo Suarez <eduardosuarez@gmail.com>
2024-05-31 21:49:21 +0200

    Add the SearchBar draft component with no logic

--- open-social-front-v2/components/layout/NavBar.js
+++ open-social-front-v2/components/layout/NavBar.js
@@ -1,9 +1,13 @@
 import Logo from "@/components/layout/Logo"
+import SearchBar from "@/components/search/SearchBar"
 
 const NavBar = () => {
   return (
     <div className="navbar sticky top-0 z-50 w-full shadow bg-white">
       <Logo visible="lg:hidden"/>
+      <div className="w-full px-4">
+        <SearchBar />
+      </div>
       <div className="w-full justify-end lg:hidden">
         <label htmlFor="drawer" aria-label="open sidebar" className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
