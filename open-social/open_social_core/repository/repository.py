5d20c2e734b120fda978e63a0896010da52f4699
Eduardo Suarez <eduardosuarez@gmail.com>
2024-11-21 20:04:30 +0100

    Remove profiling and add connection pool

--- open-social/open_social_core/repository/repository.py
+++ open-social/open_social_core/repository/repository.py
@@ -1,6 +1,5 @@
 import json
 import os
-import time
 
 import boto3
 import pymongo
@@ -24,11 +23,7 @@ def save_projects(projects):
 
 
 def get_projects(page, sorted_by, topics, languages):
-    repo_total_start = time.time()
-    get_collection_start = time.time()
     projects_collection = _get_collection('projects')
-    get_collection_end = time.time()
-    print('Get collection', get_collection_end - get_collection_start)
     results_limit = 12
     query_filter = {}
 
@@ -47,24 +42,17 @@ def get_projects(page, sorted_by, topics, languages):
     elif languages:
         query_filter.update({'language': {'$in': languages}})
 
-    repo_get_start = time.time()
     projects = list(projects_collection.find(
         filter=query_filter,
         skip=int(page if page else 0) * results_limit,
         limit=results_limit,
         sort=sorted_by
     ))
-    repo_get_end = time.time()
-    print('Repo get:', repo_get_end - repo_get_start)
 
-    repo_response_start = time.time()
     response = {
         'projects': projects
     }
-    repo_response_end = time.time()
-    print('Repo response:', repo_response_end - repo_response_start)
-    repo_total_end = time.time()
-    print('Inside Repo:', repo_total_end - repo_total_start)
+
     return response
 
 
@@ -150,6 +138,11 @@ def _get_collection(collection_name):
     secret_data = secrets_manager_client.get_secret_value(SecretId='mongodb-uri')
     uri = json.loads(secret_data['SecretString'])['mongo_db_uri']
 
-    mongo_client = pymongo.MongoClient(uri)
+    mongo_client = pymongo.MongoClient(
+        uri,
+        maxPoolSize=10,
+        minPoolSize=1
+    )
     db = mongo_client.get_database('open-social')
-    return db.get_collection(collection_name)
+    collection = db.get_collection(collection_name)
+    return collection
