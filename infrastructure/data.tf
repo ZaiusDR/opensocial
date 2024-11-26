181404702c1c3da22dc596fe804d609339551c8a
Eduardo Suarez <eduardosuarez@gmail.com>
2024-11-26 18:46:15 +0100

    Delete unneded data block

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -2,11 +2,6 @@ data "aws_route53_zone" "open_social_hosted_zone" {
   zone_id = local.open_social_hosted_zone
 }
 
-data "aws_acm_certificate" "open_social_certificate" {
-  provider = aws.us-east-1
-  domain   = "*.${trimsuffix(data.aws_route53_zone.open_social_hosted_zone.name, ".")}"
-}
-
 data "aws_iam_policy_document" "lambda_assume_role" {
   statement {
     effect  = "Allow"
