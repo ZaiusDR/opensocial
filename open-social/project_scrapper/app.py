30f9dcbe5912661a3dc8a5cfd2487670ab4a352d
Eduardo Suarez <eduardosuarez@gmail.com>
2024-12-03 18:51:10 +0100

    Fix scrapper lambda function as well

--- open-social/project_scrapper/app.py
+++ open-social/project_scrapper/app.py
@@ -1,15 +1,22 @@
 import json
 
+import pymongo
+
 from infrastructure import github_gateway
+from repository import creds_manager
 from repository import repository
 
+mongo_client = None
 
 def lambda_handler(event, context):
+    global mongo_client
+    if not mongo_client:
+        mongo_client = pymongo.MongoClient(creds_manager.get_connection_string())
     github_projects = github_gateway.get_project_list(event)
-    repository.save_languages(github_projects)
-    projects = repository.save_projects(github_projects)
+    repository.save_languages(mongo_client, github_projects)
+    projects = repository.save_projects(mongo_client, github_projects)
 
-    repository.save_topic(event['topic'])
+    repository.save_topic(mongo_client, event['topic'])
 
     return {
         "statusCode": 200,
--- open-social/tests/project_scrapper_unit/test_handler.py
+++ open-social/tests/project_scrapper_unit/test_handler.py
@@ -1,6 +1,8 @@
 import json
 import unittest
 
+import mongomock
+
 from unittest import mock
 
 from tests import fixtures
@@ -8,26 +10,33 @@ from tests import fixtures
 from project_scrapper import app
 
 
+@mongomock.patch(servers=(('localhost', 27017),))
+@mock.patch('open_social_crud.app.creds_manager.get_connection_string', return_value='mongodb://localhost:27017')
 class TestLambda(unittest.TestCase):
+
+    def setUp(self):
+        self.client = mongomock.MongoClient('localhost', 27017)
 
     @mock.patch('project_scrapper.app.github_gateway.get_project_list')
     @mock.patch('project_scrapper.app.repository.save_projects')
     @mock.patch('project_scrapper.app.repository.save_languages')
     @mock.patch('project_scrapper.app.repository.save_topic')
-    def test_should_return_a_list_of_saved_projects_using_gql(self, topic_mock, languages_mock, service_mock, gateway_mock):
+    def test_should_return_a_list_of_saved_projects_using_gql(
+            self, topics_mock, languages_mock, projects_mock, gateway_mock, mongo_mock
+    ):
         gateway_mock.return_value = {}
         event = {'topic': 'humanitarian'}
         expected_projects = fixtures.github_projects
         expected_response = {
             'statusCode': 200, 'body': json.dumps({"projects": expected_projects})
         }
-        service_mock.return_value = expected_projects
+        projects_mock.return_value = expected_projects
 
         projects = app.lambda_handler(event, {})
 
         gateway_mock.assert_called_once_with(event)
-        service_mock.assert_called_once_with({})
-        languages_mock.assert_called_once_with({})
-        topic_mock.assert_called_once_with(event['topic'])
+        projects_mock.assert_called_once_with(self.client, {})
+        languages_mock.assert_called_once_with(self.client, {})
+        topics_mock.assert_called_once_with(self.client, event['topic'])
 
         self.assertEqual(projects, expected_response)
